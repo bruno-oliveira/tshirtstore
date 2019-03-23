ae0b57b54167178c02da43e30b7d22f98f8f84cd
bruno-oliveira <olivbruno8@gmail.com>
2019-03-23 23:34:44 +0100

    Edit entry button redirects to new page

--- src/main/resources/templates/ProductController.js
+++ src/main/resources/templates/ProductController.js
@@ -1,5 +1,8 @@
-angular.module('demo', [])
-.controller('ProductController', function($scope, $http) {
+var app = angular.module('demo', []);
+
+
+
+app.controller('ProductController', function($scope, $http) {
     $http.get('http://localhost:8090/showAllProducts').
         then(function(response) {
             $scope.prods = response.data;
@@ -11,8 +14,5 @@ angular.module('demo', [])
                         console.log("URL is "+url.href);
                          $http.delete(url.href).then(alert("Item removed"));
             $scope.prods.splice(x, 1);
-
-
-
         }
 });
